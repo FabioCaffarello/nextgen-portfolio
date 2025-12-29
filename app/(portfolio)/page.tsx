7e94d80bc81addccf4b41ed64336b2fb9e2f9f98
Fabio Caffarello <fabio.caffarello@gmail.com>
2025-12-29 17:29:25 -0300

    feat(wip): first working version

---
+++ app/(portfolio)/page.tsx
@@ -0,0 +1,9 @@
+import PortifolioContent from "@/components/PortifolioContent";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <PortifolioContent />
+    </div>
+  );
+}
